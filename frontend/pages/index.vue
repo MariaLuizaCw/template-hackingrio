4bd9d0ae2441dcda4c47aac5d9ebb03461a41b98
MariaLuizaCw <marialuizacw@gmail.com>
2022-08-31 17:36:43 -0300

    initial template

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <h1>Gráficos</h1>
+        <h3>Gráfico com plotly a partir de dados de um csv</h3>
+        <h3>Gráfico com plotly a partir de dados da Api do ##</h3>
+        <h3>Gráfico com plotly a partir de um modelo de ML</h3>
+        <h1>Vantagens do Nuxt</h1>
+        <h3>Vuetify</h3>
+        <h3>Páginas dinâmicas</h3>
+
+        
+    </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
