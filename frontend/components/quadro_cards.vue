bee48eef0b652eecf35e89f54d2dbe3c3271cf80
unknown <marialuizacw@gmail.com>
2022-08-31 22:25:21 -0300

    add vuetify example

---
+++ frontend/components/quadro_cards.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="display: flex">
+       <Exemplo_vuetify_card></Exemplo_vuetify_card>
+       <Exemplo_vuetify_card></Exemplo_vuetify_card>
+
+    </div>
+</template>
+
+<script>
+
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,6 +6,12 @@
         <h3>Gráfico com plotly a partir de um modelo de ML</h3>
         <h1>Vantagens do Nuxt</h1>
         <h3>Vuetify</h3>
+        <br>
+        <div style="display: flex">
+            <Exemplo_vuetify_card></Exemplo_vuetify_card>
+            <Exemplo_vuetify_card></Exemplo_vuetify_card>
+
+        </div>
         <h3>Páginas dinâmicas</h3>
 
         
@@ -13,7 +19,9 @@
 </template>
 
 <script>
+import Exemplo_vuetify_card from '../components/exemplo_vuetify_card.vue';
+import Quadro_cards from '../components/quadro_cards.vue';
 export default {
-
+    components: { Exemplo_vuetify_card, Quadro_cards }
 }
 </script>
